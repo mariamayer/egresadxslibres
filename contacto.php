5d5d4cf45f4107330fead66411c13e127b40a404
Maria Mayer <mayermaru@gmail.com>
2019-06-21 14:07:40 -0400

    Enviar Presupuesto

--- contacto.php
+++ contacto.php
@@ -10,13 +10,44 @@ if (!$_POST){
 <?php
 }else{
 
-    $cuerpo = "Nombre: " . $_POST["nombre"] . "\r \n";
-    $cuerpo .= "Correo: " . $_POST["email"] . "\r \n";
-    $cuerpo .= "Escuela: " . $_POST["escuela"] . "\r \n";
-    $cuerpo .= "Mensaje: " . $_POST["mensaje"] . "\r \n";
+    if(isset($_POST['comments'])){
+        $comments 	 = $_POST['coments'];
 
-	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo.
-	// Si se agrega un campo al formulario, hay que agregarlo acá.
+        $cuerpo = '
+        <html>
+        <head>
+          <title>Nuevo presupuesto de '. $nombre .'</title>
+        </head>
+        <body>
+          <table class="table">
+            <tr>
+              <th align="right">E-mail:</th>
+              <td align="left">'. $email .'</td>
+            </tr>
+            <tr>
+                <th align="right">Comentarios:</th>
+                <td align="left">'. $comments .'</td>
+            </tr>';
+
+        foreach ($_POST as $key => $value) {
+            $k = htmlspecialchars($key);
+            $k = ucfirst(str_replace('-', ' ', $k));
+            if($value !== '') {
+                $cuerpo .=  '<tr><th align="right"> '.$k.' </th><td align="left"> '.htmlspecialchars($value).'</td></tr>';
+            }
+        }
+
+        $cuerpo .= '</table></body></html>';
+    } else {
+
+        $cuerpo = "Nombre: " . $_POST["nombre"] . "\r \n";
+        $cuerpo .= "Correo: " . $_POST["email"] . "\r \n";
+        $cuerpo .= "Escuela: " . $_POST["escuela"] . "\r \n";
+        $cuerpo .= "Mensaje: " . $_POST["mensaje"] . "\r \n";
+
+    	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo.
+    	// Si se agrega un campo al formulario, hay que agregarlo acá.
+    }
 
     $headers  = "MIME-Version: 1.0\n";
     $headers .= "Content-type: text/plain; charset=utf-8\n";
